f738cbc015aff1a1d1089b75643a5590d307003c
Audrea Cook <audreacwork@gmail.com>
2019-07-06 13:36:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,10 +12,17 @@ end
 
 def reduce(array, value = nil)
  # I need to ask for help. I don't understand how I got the solution before. I probably peeked.
+ if value
+   total = value
+   counter = 1
+ else
+   total = 0
+   counter = 0
+ end
   while counter < array.length do
     total += yield(array[counter])
     counter += 1
   end
-  
+
   total
 end
