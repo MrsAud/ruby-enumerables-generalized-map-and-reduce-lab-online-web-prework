e043e96de175d5de0443b1aa65fa57bc3bf42a5d
Audrea Cook <audreacwork@gmail.com>
2019-07-06 13:39:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,7 +20,7 @@ def reduce(array, value = nil)
    counter = 1
  end
   while counter < array.length do
-    total += yield(array[counter])
+    total += yield(total, array[counter])
     counter += 1
   end
 
